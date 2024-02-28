a3823b0881ab8fd6e411046219eb2612bc0a61f2
tkdgns0056 <tkdgns0056@gmail.com>
2024-02-28 19:22:00 +0900

    first commit

---
+++ src/main/java/com/project/putsalang/PutSalangApplication.java
@@ -0,0 +1,13 @@
+package com.project.putsalang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutSalangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PutSalangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/putsalang/PutSalangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.putsalang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PutSalangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
